eec801683b0022b649a03a466f12aee04081545e
Nikita <n.s.yarovyi@student.khai.edu>
2025-01-25 22:21:17 +0200

    Code optimization

--- PR01/PR01/MainForm.cs
+++ PR01/PR01/MainForm.cs
@@ -20,26 +20,16 @@ namespace PR01
             glLoadIdentity();
             glViewport(0, 0,  renderer.Width, renderer.Height);
             glOrtho(x1, x2, y1, y2, 0.0, 1.0);
-
             drawCoordinates();
-
-            double[] vertices = { -5.5, -3.5, -7.5, -2.5, -6.5, .5, -5.5, .5, -4.5, -1.5, -5.5, -3.5 };
-
             glLineWidth(8);
-
-            glBegin(GL_LINE_STRIP);
-
-            draw(vertices);
-
-            glEnd();
-
+            double[] vertices = { -5.5, -3.5, -7.5, -2.5, -6.5, .5, -5.5, .5, -4.5, -1.5, -5.5, -3.5 };
+            
+            draw(GL_LINE_STRIP, vertices);
             glEnable(GL_POINT_SMOOTH);
             glEnable(GL_BLEND);
             glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
             glPointSize(13);
-            glBegin(GL_POINTS);
-            draw(vertices, 5);
-            glEnd();
+            draw(GL_POINTS, vertices, 5);
         }
 
         private void drawCoordinates()
@@ -48,8 +38,6 @@ namespace PR01
             glLineStipple(1,0xFF00);
             glEnable(GL_LINE_STIPPLE);
             glBegin(GL_LINES);
-
-           
 
             for (double i = y1; i <= y2; i++)
             {
@@ -63,17 +51,20 @@ namespace PR01
                 }
             }
 
-
             glEnd();
             glDisable(GL_LINE_STIPPLE);
         }
 
-        private void draw(double[] vertices, int modifier = 0)
+        private void draw(uint mode, double[] vertices, int modifier = 0)
         {
+            glBegin(mode);
+
             for (int i = 0, j = 1; i < vertices.Length; i += 2, j += 2)
             {
                 glVertex2d(vertices[i] + modifier, vertices[j]);
             }
+
+            glEnd();
         }
     }
 }
