3e7682278236fad7cd554ff8fd6c269915a5ad34
Nikita <n.s.yarovyi@student.khai.edu>
2025-01-27 18:28:13 +0200

    Pr02

---
+++ PR02/PR02/MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace PR02
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            renderer = new RenderControl();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            label3 = new System.Windows.Forms.Label();
+            numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            radioButton1 = new System.Windows.Forms.RadioButton();
+            radioButton2 = new System.Windows.Forms.RadioButton();
+            radioButton3 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // renderer
+            // 
+            renderer.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            renderer.BackColor = System.Drawing.Color.White;
+            renderer.ForeColor = System.Drawing.Color.White;
+            renderer.Location = new System.Drawing.Point(12, 12);
+            renderer.Name = "renderer";
+            renderer.Size = new System.Drawing.Size(504, 332);
+            renderer.TabIndex = 0;
+            renderer.TextCodePage = 1251;
+            renderer.Render += render2;
+            // 
+            // label1
+            // 
+            label1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.Location = new System.Drawing.Point(570, 12);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(99, 25);
+            label1.TabIndex = 1;
+            label1.Text = "Tile count:";
+            // 
+            // label2
+            // 
+            label2.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label2.Location = new System.Drawing.Point(527, 46);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(98, 25);
+            label2.TabIndex = 2;
+            label2.Text = "horizontal";
+            // 
+            // label3
+            // 
+            label3.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label3.Location = new System.Drawing.Point(527, 85);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(73, 25);
+            label3.TabIndex = 3;
+            label3.Text = "vertical";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            numericUpDown1.Location = new System.Drawing.Point(655, 39);
+            numericUpDown1.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new System.Drawing.Size(38, 32);
+            numericUpDown1.TabIndex = 4;
+            numericUpDown1.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            numericUpDown2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            numericUpDown2.Location = new System.Drawing.Point(655, 83);
+            numericUpDown2.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new System.Drawing.Size(38, 32);
+            numericUpDown2.TabIndex = 5;
+            numericUpDown2.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            numericUpDown2.ValueChanged += numericUpDown2_ValueChanged;
+            // 
+            // radioButton1
+            // 
+            radioButton1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            radioButton1.Location = new System.Drawing.Point(529, 315);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new System.Drawing.Size(127, 29);
+            radioButton1.TabIndex = 6;
+            radioButton1.Text = "Point Mode";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // radioButton2
+            // 
+            radioButton2.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            radioButton2.Location = new System.Drawing.Point(529, 280);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new System.Drawing.Size(119, 29);
+            radioButton2.TabIndex = 7;
+            radioButton2.Text = "Line Mode";
+            radioButton2.UseVisualStyleBackColor = true;
+            radioButton2.CheckedChanged += radioButton2_CheckedChanged;
+            // 
+            // radioButton3
+            // 
+            radioButton3.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            radioButton3.AutoSize = true;
+            radioButton3.Checked = true;
+            radioButton3.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            radioButton3.Location = new System.Drawing.Point(529, 245);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new System.Drawing.Size(108, 29);
+            radioButton3.TabIndex = 8;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "Fill Mode";
+            radioButton3.UseVisualStyleBackColor = true;
+            radioButton3.CheckedChanged += radioButton3_CheckedChanged;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(705, 377);
+            Controls.Add(radioButton3);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(renderer);
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            Name = "MainForm";
+            Text = "OpenGL Application";
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private RenderControl renderer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+    }
+}
+
---
+++ PR02/PR02/MainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Windows.Forms;
+using static PR02.OpenGL;
+
+
+namespace PR02
+{
+    public partial class MainForm : Form
+    {
+        const float TileSize = 3.0f;
+        int tilesHorizontal = 1;
+        int tilesVertical = 1;
+        uint drawMode = GL_FILL;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void render2(object sender, System.EventArgs e)
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            int zoom = 10;
+            gluOrtho2D(-renderer.Width / zoom, renderer.Width / zoom, -renderer.Height / zoom, renderer.Height / zoom);
+            glViewport(0, 0, renderer.Width, renderer.Height);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+
+
+            glPolygonMode(GL_FRONT_AND_BACK, drawMode);
+
+            DrawHexagonGrid(tilesVertical, tilesHorizontal);
+        }
+
+        private void DrawHexagonGrid(int rows, int cols)
+        {
+            float halfSize = TileSize / 2;
+            float width = (float)Math.Sqrt(3) * halfSize;
+            float height = 3.3f * halfSize;
+            float xOffset = -cols * width / 2;
+            float yOffset = -rows * height / 2;
+            glTranslatef(xOffset, yOffset, 0);
+
+            for (int row = 0; row < rows; row++)
+            {
+                for (int col = 0; col < cols; col++)
+                {
+                    float x = col * width * 1.7f;
+                    float y = row * height + (col % 2) * height / 2;
+
+                    drawTile(x, y);
+                }
+            }
+        }
+
+        void drawTile(float centerX, float centerY)
+        {
+            int numSides = 6;
+            double angleStep = Math.PI * 2 / numSides;
+            glBegin(GL_TRIANGLE_FAN);
+            glColor3f(1, 0, 0);
+            glVertex2f(centerX, centerY);
+
+            glColor3f(0, 1, 0);
+            for (int i = 0; i <= numSides; i++)
+            {
+                double angle = i * angleStep;
+                float x = (float)(centerX + (TileSize * Math.Cos(angle)));
+                float y = (float)(centerY + (TileSize * Math.Sin(angle)));
+                glVertex2f(x, y);
+            }
+            glEnd();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            tilesHorizontal = Convert.ToInt32(numericUpDown1.Value);
+            Refresh();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            tilesVertical = Convert.ToInt32(numericUpDown2.Value);
+            Refresh();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMode = GL_FILL;
+            Refresh();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMode = GL_LINE;
+            Refresh();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            drawMode = GL_POINT;
+            Refresh();
+        }
+    }
+}
